144a76d42e8af8568c0136d52e74be463a1e96c9
SeungminYun-G-B <147472555+SeungminYun-G-B@users.noreply.github.com>
2023-11-05 21:50:25 +0900

    중력 값 변경
    원하는 그림은 아래로 떨어지는 게 아니어서 사실상 applyForce가 필요하지 않았음

--- particle.js
+++ particle.js
@@ -1,6 +1,6 @@
 class particle{
     constructor(position){
-        this.acceleration = createVector(0, 0.05);
+        this.acceleration = createVector(0, 0);
         this.velocity = createVector(random(-1, 1), random(-1, 0));
         this.position = position.copy();
         this.lifespan = random(255);
